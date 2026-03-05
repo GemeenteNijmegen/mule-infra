16d4d3b6502b8b56cdd4bb28019d343d35e9d017
Teppei Kamimaru <t1.kamimaru@nijmegen.nl>
2026-03-05 13:49:25 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,14 @@
+import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.189.1',
+  defaultReleaseBranch: 'main',
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  makeSampleFiles: true,
+  name: 'mule-infra',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
