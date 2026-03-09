6c367fd328242562561ce3d0d89e1b5546ca9360
Esper Kuijs <esper_kuijs@hotmail.com>
2026-03-09 11:55:11 +0100

    chore: rename default stack to MuleRuntimeStack

--- src/MuleRuntimeStack.ts
+++ src/MuleRuntimeStack.ts
@@ -1,14 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { Configurable } from './Configuration';
+import { Statics } from './Statics';
 
 interface MainStackProps extends StackProps, Configurable { }
 
 export class MainStack extends Stack {
   constructor(scope: Construct, id: string, private readonly props: MainStackProps) {
     super(scope, id, props);
-
-    // TODO add resources here
-
   }
 }
