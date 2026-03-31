e0aeaf4d4e4c51bd80e0ba9b7efc9e6d52a60ebe
Esper Kuijs <esper_kuijs@hotmail.com>
2026-03-31 10:53:07 +0200

    feat: intial setup for mule runtime cluster, service and task definition, retrieving image from ECR gn-build

--- src/MuleRuntimeStack.ts
+++ src/MuleRuntimeStack.ts
@@ -1,7 +1,9 @@
-import { Aspects, Stack, StackProps } from 'aws-cdk-lib';
+import { GemeenteNijmegenVpc, PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
+import { Aspects, Stack, StackProps, aws_ecs as ecs, aws_ec2 as ec2 } from 'aws-cdk-lib';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import { FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
 import { Configurable } from './Configuration';
-import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
 
 interface MuleRuntimeStackProps extends StackProps, Configurable { }
 
@@ -9,11 +11,39 @@ export class MuleRuntimeStack extends Stack {
   constructor(scope: Construct, id: string, private readonly props: MuleRuntimeStackProps) {
     super(scope, id, props);
     Aspects.of(this).add(new PermissionsBoundaryAspect());
+    const vpc = new GemeenteNijmegenVpc(this, 'vpc');
 
-    // cluster definieren
-    // Task definition
+    const cluster = new ecs.Cluster(this, 'MuleRuntimeCluster', {
+      vpc: vpc.vpc,
+    });
+
+    const muleRuntimeEcr = ecr.Repository.fromRepositoryArn(this, 'MuleDockerImageRepository', 'arn:aws:ecr:eu-central-1:836443378780:repository/mule-docker-image');
+
+    const taskDefinition: FargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'MuleRuntimeTaskDefinition');
+    const container = taskDefinition.addContainer('MuleRuntimeContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(muleRuntimeEcr, 'latest'),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'mule-runtime' }),
+    });
+
+    container.addPortMappings({
+      containerPort: 443,
+      protocol: ecs.Protocol.TCP,
+    });
+
+    container.addPortMappings({
+      containerPort: 8080,
+      protocol: ecs.Protocol.TCP,
+    });
+
+    new ecs.FargateService(this, 'Service', {
+      cluster,
+      taskDefinition,
+      minHealthyPercent: 100,
+      // add to a subnet with internet access
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+    });
+
     // service definieren
-    // VPC regelen met security groups etc.
     // Image van ECR pullen
   }
 }
