2a2a2249ce3cd6f61fb013c6369ef08232923e77
Teppei Kamimaru <t1.kamimaru@nijmegen.nl>
2026-06-18 14:05:38 +0200

    chore: update docker image version

--- src/MuleRuntimeStack.ts
+++ src/MuleRuntimeStack.ts
@@ -102,7 +102,7 @@ export class MuleRuntimeStack extends Stack {
       }));
 
       const container = taskDefinition.addContainer('MuleRuntimeContainer', {
-        image: ecs.ContainerImage.fromEcrRepository(muleRuntimeEcr, 'c799bd15edd0a6900481be61fa7297e8c5f1a88f'),
+        image: ecs.ContainerImage.fromEcrRepository(muleRuntimeEcr, 'de1b2ea256fc71899efecff4b500d003b39d2e73'),
         logging: ecs.LogDrivers.awsLogs({ streamPrefix: `mule-runtime-${i}` }),
         environment: {
           SECRET_MULE_LICENSE_ARN: licenseSecret.secretArn,
