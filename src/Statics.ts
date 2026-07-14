09b5f0555161391faa7c553b2ac48facd941963b
Esper Kuijs <esper_kuijs@hotmail.com>
2026-07-14 09:43:03 +0200

    chore: update docker image to fix issues with EFS volume reset

--- src/Statics.ts
+++ src/Statics.ts
@@ -49,6 +49,6 @@ export class Statics {
   static readonly secretMuleTrustStore = `/${Statics.projectName}/mule/truststore`;
   static readonly secretMuleKeyStore = `/${Statics.projectName}/mule/keystore`;
   static readonly muleDockerImageRepositoryArn = 'arn:aws:ecr:eu-central-1:836443378780:repository/mule-docker-image';
-  static readonly muleDockerImageHash = '1351c7bd924d58273839622d374d8f32a49b92da';
+  static readonly muleDockerImageHash = '041179457ccaa6383aeac15d6214c4a33c773ccf';
 
 }
